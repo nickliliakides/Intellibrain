9819687a45aca369ff72b8c0d86000ac222b20c0
Nick Lils <nicklils@Nicks-Mac-mini.local>
2018-12-31 22:07:16 +0000

    Updates

---
+++ precache-manifest.d84773eced47d5aa41bcd57aafd03029.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be19df197dcc3f328a84",
+    "url": "/Intellibrain/static/js/runtime~main.be19df19.js"
+  },
+  {
+    "revision": "cd12387c9e4ca6464b61",
+    "url": "/Intellibrain/static/js/main.cd12387c.chunk.js"
+  },
+  {
+    "revision": "8aaeb0953cd898a2834a",
+    "url": "/Intellibrain/static/js/1.8aaeb095.chunk.js"
+  },
+  {
+    "revision": "cd12387c9e4ca6464b61",
+    "url": "/Intellibrain/static/css/main.3defc323.chunk.css"
+  },
+  {
+    "revision": "8aaeb0953cd898a2834a",
+    "url": "/Intellibrain/static/css/1.b93452dd.chunk.css"
+  },
+  {
+    "revision": "9f86715720df59497e3ffd2554e626a8",
+    "url": "/Intellibrain/index.html"
+  }
+];
